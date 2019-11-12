ecddb2353b804743a76189adc0f057bb4bdc66e9
Vove <1132412166@qq.com>
2019-11-12 11:11:55 +0800

    fix电源提醒词错误

--- app/src/main/java/cn/vove7/jarvis/plugins/PluginConfig.kt
+++ app/src/main/java/cn/vove7/jarvis/plugins/PluginConfig.kt
@@ -14,7 +14,7 @@ import cn.vove7.smartkey.key.noCacheKey
 object PluginConfig : AConfig() {
 
     val onFullText by noCacheKey("吃饱了 吃饱了", "full_power_hint_text")
-    val onLowText by noCacheKey("吃饱了 吃饱了", "low_power_hint_text")
+    val onLowText by noCacheKey("饿死了 饿死了", "low_power_hint_text")
     val onChargingText by noCacheKey("吃饭了 吃饭了", "charging_hint_text")
 
 }
