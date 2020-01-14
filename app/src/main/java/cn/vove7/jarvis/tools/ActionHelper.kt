b7bf0f1ed11756a6ba1c635b2460cf174490f803
Vove <1132412166@qq.com>
2020-01-14 13:43:59 +0800

    [屏幕助手] 优化微信扫一扫

--- app/src/main/java/cn/vove7/jarvis/tools/ActionHelper.kt
+++ app/src/main/java/cn/vove7/jarvis/tools/ActionHelper.kt
@@ -33,30 +33,28 @@ object ActionHelper {
     /**
      * 使用微信扫描屏幕
      */
-    fun qrWithWechat(path: String) {
-        CoroutineExt.launch {
-            var tmpFile: File? = null
-            try {
-                tmpFile = createTmpFile
-                File(path).copyTo(tmpFile, true)//复制文件
-                tmpFile.broadcastImageFile()
+    fun qrWithWechat(path: String) = CoroutineExt.launch {
+        var tmpFile: File? = null
+        try {
+            tmpFile = createTmpFile
+            File(path).copyTo(tmpFile, true)//复制文件
+            tmpFile.broadcastImageFile()
 
-                doQrWithWechat()
-            } catch (e: Throwable) {
-                tmpFile?.delete()
-                tmpFile = null
-                GlobalApp.toastError("执行失败: " + e.message)
-            } finally {
-                //立即删除会导致无法识别
-                delay(1000)
-                tmpFile?.delete()
-            }
+            doQrWithWechat()
+        } catch (e: Throwable) {
+            tmpFile?.delete()
+            tmpFile = null
+            GlobalApp.toastError("执行失败: " + e.message)
+        } finally {
+            //立即删除会导致无法识别
+            delay(2000)
+            tmpFile?.delete()
         }
     }
 
     val app get() = GlobalApp.APP
 
-    private fun doQrWithWechat() {
+    private suspend fun doQrWithWechat() {
         val intent = app.packageManager.getLaunchIntentForPackage("com.tencent.mm")
         if (intent == null) {
             GlobalApp.toastError("未安装微信")
@@ -67,7 +65,7 @@ object ActionHelper {
         app.startActivity(intent.clearTask())
 
         text("扫二维码 / 条码 / 小程序码").parent!!.children[2].click()
-
+        delay(800)
         type("RecyclerView").children[1].click()
     }
 
@@ -75,28 +73,26 @@ object ActionHelper {
      * 调用支付宝扫一扫
      * @param path String
      */
-    fun qrWithAlipay(path: String) {
-        CoroutineExt.launch {
-            try {
-                if (SystemBridge.getAppInfo("com.eg.android.AlipayGphone") == null) {
-                    GlobalApp.toastError("未安装支付宝")
-                    return@launch
-                }
-                val tmpFile = File(path)
-
-                val imgUri = if (Build.VERSION.SDK_INT >= 24) {
-                    FileProvider.getUriForFile(app, "${app.packageName}.fileprovider", tmpFile)
-                } else Uri.fromFile(tmpFile)
-                val intent = Intent(Intent.ACTION_SEND).apply {
-                    type = "image/jpg"
-                    putExtra(Intent.EXTRA_STREAM, imgUri)
-                    component = ComponentName("com.eg.android.AlipayGphone", "com.alipay.mobile.quinox.splash.ShareScanQRDispenseActivity")
-                }
-                app.startActivity(intent.newTask())
-
-            } catch (e: Throwable) {
-                GlobalApp.toastError("执行失败: " + e.message)
+    fun qrWithAlipay(path: String) = CoroutineExt.launch {
+        try {
+            if (SystemBridge.getAppInfo("com.eg.android.AlipayGphone") == null) {
+                GlobalApp.toastError("未安装支付宝")
+                return@launch
             }
+            val tmpFile = File(path)
+
+            val imgUri = if (Build.VERSION.SDK_INT >= 24) {
+                FileProvider.getUriForFile(app, "${app.packageName}.fileprovider", tmpFile)
+            } else Uri.fromFile(tmpFile)
+            val intent = Intent(Intent.ACTION_SEND).apply {
+                type = "image/jpg"
+                putExtra(Intent.EXTRA_STREAM, imgUri)
+                component = ComponentName("com.eg.android.AlipayGphone", "com.alipay.mobile.quinox.splash.ShareScanQRDispenseActivity")
+            }
+            app.startActivity(intent.newTask())
+
+        } catch (e: Throwable) {
+            GlobalApp.toastError("执行失败: " + e.message)
         }
     }
 
