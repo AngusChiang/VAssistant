e878fa4e02d7a4bd6e674c4dc5e6d950f34aa786
Vove <1132412166@qq.com>
2020-04-21 14:40:37 +0800

    2.0.8.1

--- app/src/main/java/cn/vove7/jarvis/activities/OSLActivity.kt
+++ app/src/main/java/cn/vove7/jarvis/activities/OSLActivity.kt
@@ -39,6 +39,7 @@ class OSLActivity : OneFragmentActivity() {
             val l = listOf(
                     OslItem("GreenDao", "https://github.com/greenrobot/greenDAO", "greenDAO is an open source Android ORM making development for SQLite databases fun again. It relieves developers from dealing with low-level database requirements while saving development time.")
                     , OslItem("EventBus", "https://github.com/greenrobot/EventBus", "EventBus is an open-source library for Android and Java using the publisher/subscriber pattern for loose coupling. EventBus enables central communication to decoupled classes with just a few lines of code – simplifying the code, removing dependencies, and speeding up app development.")
+                    , OslItem("ParamRegexNdk", "https://github.com/Vove7/ParamRegexNdk", "匹配形如 `@{city}的?天气` 的'正则式'")
                     , OslItem("OkHttp", "https://github.com/square/okhttp", "HTTP is the way modern applications network. It’s how we exchange data & media. Doing HTTP efficiently makes your stuff load faster and saves bandwidth.")
                     , OslItem("Gson", "https://github.com/google/gson", "Gson is a Java library that can be used to convert Java Objects into their JSON representation. It can also be used to convert a JSON string to an equivalent Java object. Gson can work with arbitrary Java objects including pre-existing objects that you do not have source-code of.")
                     , OslItem("AndroLua", "https://github.com/L-JINBIN/AndroidLua", "AndroLua is the Lua interpreter ported to the Android platform.")
@@ -61,7 +62,6 @@ class OSLActivity : OneFragmentActivity() {
                     , OslItem("Recycler Fast Scroll", "https://github.com/plusCubed/recycler-fast-scroll", "")
                     , OslItem("MarkdownView", "https://github.com/tiagohm/MarkdownView", "Android library to display markdown text.")
                     , OslItem("Toasty", "https://github.com/GrenderG/Toasty", "The usual Toast, but with steroids.")
-//                    , OslItem("Konfetti", "https://github.com/DanielMartinus/Konfetti", "Celebrate more with this lightweight confetti particle system \uD83C\uDF8A Create realistic confetti by implementing this easy to use library.")
                     , OslItem("Luban", "https://github.com/Curzibn/Luban", "Luban（鲁班） —— Android图片压缩工具，仿微信朋友圈压缩策略。")
                     // , OslItem("", "", "")
             )
